3b4ac277043a8164408682c7ce7617907316519c
Daniel Roman <daniel.roman@csedge.org>
2018-10-06 15:45:38 -0400

    Completed Challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,31 @@
 // Example
-let message = "Hello World!";
-console.log(message);
+
+
 
 // Type your code below!
+let Age = 15;
+let Grade = 9;
+let Siblings = 3;
+let Pets = false;
+let SocialSecurityNumber = undefined;
+let Damage = true;
+let Dead = 18.5;
+let McDonaldsWorker = "Hello, thank you for choosing McDonalds how can I help you today?";
+let Customer = "Can I have a big mac with some french fries? And a cheeseburger please.";
+console.log("GoodEvening");
+console.log(McDonaldsWorker);
+console.log(Customer);
+console.log(Dead);
+console.log(Damage);
+console.log(Age);
+console.log(Grade);
+console.log(Siblings);
+console.log(Pets);
+console.log(SocialSecurityNumber);
+
+const READLINE = require("readline-sync");
+
+let userInput = READLINE.question("Type anything and I'll repeat it back!");
+
+console.log(userInput);
+
