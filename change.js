9c42c3440927857bd1f36b8cc8880189f47e07dc
Daniel Roman <daniel.roman@csedge.org>
2018-10-20 15:46:23 -0400

    Object

--- change.js
+++ change.js
@@ -2,6 +2,29 @@
 const READLINE = require("readline-sync");
 
 // fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+let changeMachine = {
+	inputCents: "y",
+    quarters: "quarters",
+    dimes: "dimes",
+    nickels: "nickels",
+    pennies: "pennies",
+    greeting: "Hey, this is Bob. Bob the change machine, please input the amount of cents as a positive integer.",
+    farewell: "Thank you very much for taking money out of me! Come again."
+};
 
 // write code below that simulates the change machine in action!
+let y = READLINE.question ("Hey, this is Bob. Bob the change machine, please input the amount of cents as a positive integer: ");
+
+let quarters = Math.floor(y/25);
+let remainder = Math.floor(y%25);
+let dimes = Math.floor(remainder/10);
+remainder = Math.floor(remainder%10);
+let nickels = Math.floor(remainder/5);
+remainder = Math.floor(remainder%5);
+let pennies = remainder;
+console.log(`${y} cents would make`);
+console.log(`quarters = ${quarters}`);
+console.log(`dimes = ${dimes}`);
+console.log(`nickels = ${nickels}`);
+console.log(`pennies = ${pennies}`);
+
---
+++ object.js
@@ -0,0 +1,37 @@
+let villain = {
+	firstName: "Donald",
+	lastName: "Crump",
+	nickName: "Crumpy",
+	likesMexicans: false,
+	howOld: "Very",
+	CatchPhrase: "I like walls that are tall.",
+	Crime: "Bank Robbing for a small loan of a million dollars!"
+
+};
+
+let spy = {
+	firstName: "Agent",
+	lastName: "Perry",
+	nickName: "Agent P",
+	catchPhrase: "Agent Perry is always hairy!",
+	Archenemy: "Crumpy"
+
+};
+console.log(`Villain's first name: ${villain.firstName}
+Villain's last Name: ${villain.lastName}
+Villain's nick Name: ${villain.nickName}
+Villain's CatchPhrase: ${villain.CatchPhrase}
+Villain's Crime: ${villain.Crime}`); 
+
+
+console.log(`Spy's first name: ${spy.firstName}
+Spy's last name: ${spy.lastName}
+Spy's nick Name: ${spy.nickName}
+Spy's CatchPhrase: ${spy.CatchPhrase}
+Spy's Archenemy: ${spy.Archenemy}`); 
+
+console.log("Hello people of earth, this is Agent Perry, you can call him Agent P for short. But we just call him P for shorter. Agent Perry has a passion for fighting crime, even though all his family members have committed crimes and are or have been in jail. Agent Perry has 2 brothers and a sister, a mother and a father. P has always been independent and on his own and doesn't like nor appreciates help from others. P's famous catchphrase is: Agent Perry is always hairy! We're not sure why though");
+
+
+console.log("Agent Perry's archenemy is Donald Crump who he despises because Donald has always been sneaky and seems to not care for people");
+
